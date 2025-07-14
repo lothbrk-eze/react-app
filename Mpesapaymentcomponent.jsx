169bd829593f2a3c6582da45bf4aadae9bb04023
lothbrk-eze <ezekielkiprop101@gmail.com>
2025-07-14 12:36:50 +0300

    Update Mpesapaymentcomponent.jsx

--- Mpesapaymentcomponent.jsx
+++ Mpesapaymentcomponent.jsx
@@ -1,24 +1,58 @@
-import React from 'react'
+import axios from 'axios'
+import React,{useState}  from 'react'
+import { useLocation } from 'react-router-dom'
 
 const Mpesapaymentcomponent = () => {
+  const imagepath="https://bombadir.pythonanywhere.com/static/images/"
+  const {product} = useLocation().state  || {}
+  const[phone,setPhone]=useState("")
+  const[amount,setAmount]=useState("")
+
+  // 3 states of posting data
+  const[loading,setLoading]=useState("")
+  const[success,setSuccess]=useState("")
+  const[error,setError]=useState("")
+// function to post data
+const submit = async (e) => {
+  e.preventDefault()
+  setLoading("Please wait....")
+  try {
+    // initialize a new envelope
+    const formdata= new FormData()
+    formdata.append("phone",phone)
+    formdata.append("amount","1")
+    const response= await axios.post(" https://ryw.pythonanywhere.com/api/mpesa_payment ",formdata)
+
+    setLoading("")
+    setSuccess(response.data.message)
+  } catch (error) {
+    
+  }
+}
+
   return (
  <div className='row justify-content-center'>
   <div className=' col-md-6 card shadow mt-4 '>
-    <img src="" alt="" /> <br />
-    <b className='text-start'>Daly maziwa</b> <br />
-    <p className='text-start'>this is a good product</p> <br />
-    <b className='text-start'>Ksh 300</b> <br />
+    <img src={imagepath + product.product_photo } alt="" /> <br />
+ 
+    <h3>{product.product_name}</h3>
 
-    <input type="number"
-    placeholder='enter phone(2547XXXXXXXX)'
-    className='text-start'
-     /> <br />
-    
-     <div className='card-footer'>
-     <input type="submit"
-     value={"Make Payment"}
-     className='btn btn-success w-100 text-start' />
-     </div>
+  
+    <b className='text-start'>{product.product_cost}</b> <br />
+
+    <form action="" onSubmit={submit}>
+      <input type="number"
+      placeholder='Enter your number 2547XXXXXXXX'
+      className='form-control'
+      onChange={ (e)=>setPhone(e.target.value)} /> <br />
+      <input type="submit"
+      value={"Make payment"}
+      className='btn btn-success w-100' />
+      <br />
+      <h2 className="text-warning"> {loading}</h2>
+    <h2 className="text-success"> {success}</h2>
+    <h2 className="text-danger">{error}</h2>
+    </form>
 
   </div>
 
